5c0e24e46d8d393b1d25a6c462e5204ba9044001
momentnwask <160561004+nwaskksayway@users.noreply.github.com>
2025-04-13 19:25:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 
     if (password === correctPassword) {
         // Redirecionar para outra página (pode ser alterado conforme sua necessidade)
-        window.location.href = "https://deskpaymoment.github.io/TOOLS.nw/";
+        window.location.href = "https://nwaskksayway.github.io/TOOLS.nw/";
     } else {
         document.getElementById("error-message").textContent = "Senha incorreta. Tente novamente.";
     }
